756eb5ccfadf585dd85a9f28d835772fb5401e0e
shipalibhandary <shipalibhandary@gmail.com>
2026-02-04 14:30:00 +0530

    Adding splash page with logo

---
+++ app/splash.tsx
@@ -0,0 +1,48 @@
+import { useRouter } from "expo-router";
+import { useEffect } from "react";
+import { ActivityIndicator, Image, StyleSheet, Text, View } from "react-native";
+
+export default function Splash() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace("/login");
+    }, 1000); // 2 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../assets/images/favicon.png")}
+        style={styles.logo}
+      />
+
+      <Text style={styles.appName}>My App</Text>
+
+      <ActivityIndicator size="large" color="#000" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+  },
+  logo: {
+    width: 120,
+    height: 120,
+    marginBottom: 20,
+    resizeMode: "contain",
+  },
+  appName: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+});
